4f54d099ee0172ccec0f4c67c10239ec99d8024f
pnarsina <pnarsina@yahoo.com>
2020-11-11 05:45:13 +0000

    made minor modifications to make configuration file work

--- project_re/model/MedClinical.py
+++ project_re/model/MedClinical.py
@@ -1,15 +1,23 @@
 import torch.nn as nn
 from transformers import BertModel
-from config import model_config 
+# from .model_config import model_config 
 
+class model_config:
+    def __init__(self):
+        self.bert_features = 768
+        self.layer1_features = 256
+        self.label_classes = 42
+        
 class Biobert_fc(nn.Module):
     
     def __init__(self):
         super(Biobert_fc, self).__init__()
-        
+
+        self.model_conf = model_config()
+
         self.bert = BertModel.from_pretrained("gsarti/biobert-nli")
-        self.linear1 = nn.Linear(model_config.bert_features, model_config.layer1_features)
-        self.linear2 = nn.Linear(model_config.layer1_features, model_config.label_classes) ## 3 is the number of classes in this example
+        self.linear1 = nn.Linear(self.model_conf.bert_features, self.model_conf.layer1_features)
+        self.linear2 = nn.Linear(self.model_conf.layer1_features, self.model_conf.label_classes) ## 3 is the number of classes in this example
         
     def forward(self, ids, segment_ids, mask):
           sequence_output, pooled_output = self.bert(
@@ -18,7 +26,7 @@ class Biobert_fc(nn.Module):
                attention_mask=mask)
  
           # sequence_output has the following shape: (batch_size, sequence_length, 768)
-          linear1_output = self.linear1(sequence_output[:,0,:].view(-1,model_config.bert_features)) ## extract the 1st token's embeddings
+          linear1_output = self.linear1(sequence_output[:,0,:].view(-1,self.model_conf.bert_features)) ## extract the 1st token's embeddings
  
           linear2_output = self.linear2(linear1_output)
  
