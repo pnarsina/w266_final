db62500977684052d3372857878d00eb9218a844
= <=>
2020-11-20 22:08:30 -0800

    modified config class to be global instead of local to method, so that we can save with pickle

--- project_re/util/tools.py
+++ project_re/util/tools.py
@@ -5,16 +5,18 @@ import logging
 import json
 import config
 
+logger = logging.getLogger()
+
+class config(object):
+    def __init__(self, d):
+        for a, b in d.items():
+            if isinstance(b, (list, tuple)):
+               setattr(self, a, [config(x) if isinstance(x, dict) else x for x in b])
+            else:
+               setattr(self, a, config(b) if isinstance(b, dict) else b)
 
 def load_config(config_folder):
-    class obj(object):
-        def __init__(self, d):
-            for a, b in d.items():
-                if isinstance(b, (list, tuple)):
-                   setattr(self, a, [obj(x) if isinstance(x, dict) else x for x in b])
-                else:
-                   setattr(self, a, obj(b) if isinstance(b, dict) else b)
             
     with open(os.path.join(config_folder, "config.json")) as f:
-        config = obj(json.load(f))
-    return config
+        config1 = config(json.load(f))
+    return config1
